af3a1a5fa946028bc8d0c58f33498bb4ac83b0c9
BIT student <classroom@bgit.rs>
2018-08-11 13:46:56 +0200

    progress

---
+++ js/dataModule.js
@@ -0,0 +1,22 @@
+class Book {
+    constructor(obj) {
+        this.id = obj.id
+        this.title = obj.volumeInfo.title
+        this.author = obj.volumeInfo.authors[0]
+        this.publishedDate = obj.volumeInfo.publishedDate
+        this.pageCount = obj.volumeInfo.pageCount
+        this.description = obj.volumeInfo.description
+        this.categories = obj.volumeInfo.categories[0]
+        this.averageRating = obj.volumeInfo.averageRating
+        this.smallThumbnail = obj.volumeInfo.imageLinks.smallThumbnail
+        this.thumbnail = obj.volumeInfo.imageLinks.thumbnail
+    }
+}
+
+const booksArr = (obj) => {
+    return obj.items.map((el) => {
+        return new Book(el)
+    })
+}
+
+export default booksArr
