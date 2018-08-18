c73787b98ba9fc418308de8f0cba38a28942668c
BIT student <classroom@bgit.rs>
2018-08-18 22:08:52 +0200

    mobile finished

--- js/UIModule.js
+++ js/UIModule.js
@@ -3,3 +3,9 @@ const info = document.querySelector('.info')
 homeIcon.addEventListener('click', () => {
     info.style.display = 'block'
 })
+
+const loginIcon = document.querySelector('.login-icon')
+const login = document.querySelector('.login-wrapper')
+loginIcon.addEventListener('click', () => {
+    login.style.display = 'block'
+})
