353e3cf61f214b5b75337cab299f92913210d481
BIT student <classroom@bgit.rs>
2018-08-19 03:34:02 +0200

    tablet finished

--- js/UIModule.js
+++ js/UIModule.js
@@ -1,11 +1,26 @@
 const homeIcon = document.querySelector('.home-icon')
 const info = document.querySelector('.info')
 homeIcon.addEventListener('click', () => {
-    info.style.display = 'block'
+    info.classList.toggle('block')
 })
 
 const loginIcon = document.querySelector('.login-icon')
 const login = document.querySelector('.login-wrapper')
 loginIcon.addEventListener('click', () => {
-    login.style.display = 'block'
+    login.classList.toggle('block')
 })
+
+const comments = document.querySelector('.comments')
+const commentsIcon = document.querySelector('.comments-icon')
+
+commentsIcon.addEventListener('click', () => {
+    comments.classList.toggle('block')
+})
+
+const cartWrapperMobile = document.querySelector('.cart-wrapper-mobile')
+const cartIcon = document.querySelector('.cart-icon')
+
+cartIcon.addEventListener('click', () => {
+    cartWrapperMobile.classList.toggle('block')
+})
+
